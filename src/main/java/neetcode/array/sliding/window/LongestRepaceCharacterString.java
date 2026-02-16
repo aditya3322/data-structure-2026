626a7bc480050d0eeb57632158ca93f109062deb
Aditya Awasthi <aditya3322@gmail.com>
2026-02-16 21:21:35 +0530

    Implement LongestRepaceCharacterString algorithm with main method

--- src/main/java/neetcode/array/sliding/window/LongestRepaceCharacterString.java
+++ src/main/java/neetcode/array/sliding/window/LongestRepaceCharacterString.java
@@ -1,21 +1,34 @@
 package neetcode.array.sliding.window;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
 class LongestRepaceCharacterString {
 
+    public int characterReplacement(String s, int k) {
+        int[] charCount = new int[26];
+        int left = 0;
+        int maxLen = 0;
+        int maxCount = 0;
 
-//    public int characterReplacement(String s, int k) {
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        String input  = "XXYYZ";
-//        int k = 2;
-//    }
+        for (int right = 0; right < s.length(); right++) {
+            charCount[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, charCount[s.charAt(right) - 'A']);
 
+            while (right - left + 1 - maxCount > k) {
+                charCount[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String input = "XXYYZ";
+        int k = 2;
+        LongestRepaceCharacterString obj = new LongestRepaceCharacterString();
+        System.out.println(obj.characterReplacement(input, k));
+    }
 
 }
 
